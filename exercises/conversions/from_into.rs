0315a6193950440319d59e8c8b0bd4d828574eb6
liangyongrui <liangyongrui@kuaishou.com>
2022-11-02 21:10:04 +0800

    finish

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,33 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let mut i = s.split(",").into_iter();
+        let name = if let Some(name) = i.next() {
+            name
+        } else {
+            return Person::default();
+        };
+        if name.len() == 0 {
+            return Person::default();
+        }
+        let age = if let Some(age) = i.next() {
+            if let Ok(age) = age.parse() {
+                age
+            } else {
+                return Person::default();
+            }
+        } else {
+            return Person::default();
+        };
+        if i.next().is_some() {
+            return Person::default();
+        }
+        Person {
+            name: name.to_owned(),
+            age,
+        }
     }
 }
 
